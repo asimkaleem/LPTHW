b22d6e109b8181adf3ae7c1c1924ac78b55e747e
asim kaleem khan <asim@ebryx.com>
2018-03-20 18:16:42 +0500

    Adding third exercise

---
+++ ex3.py
@@ -0,0 +1,17 @@
+# prints a simple statement
+print " I will now count my chickens:"
+# prints a string along with Hens count by adding 25 into 30 and
+print "Hens", 25 + 30 / 6
+# print the
+print "Roosters", 100 - 25 * 3 % 4, 75%4
+print "Now i will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 /  4 + 6
+print "Is it true that 3 + 2 < 5 - 7 ?"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2 ?", 3 + 2
+print "What is 5 - 7 ?", 5 - 7
+print "Ohh thats why its False."
+print "How about some more."
+print "Is it greater", 5 > -2
+print "Is it greater or equal", 5 >= -2
+print "Is it less or equal", 5 <= -2
