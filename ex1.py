508a4c656f8af926fa0b5796a7397ffd8bb158b3
asim kaleem khan <asim@ebryx.com>
2018-03-20 18:08:01 +0500

    Adding first exercise

---
+++ ex1.py
@@ -0,0 +1,19 @@
+print "Hello World!"
+print "Hello Again"
+print "I Like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Study Drill # 1
+print "this is extra line which study drill 1 asked"
+
+# Study drill # 2
+print "Hello World!"
+# print "Hello Again"
+# print "I Like typing this."
+# print "This is fun."
+# print "Yay! Printing."
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
